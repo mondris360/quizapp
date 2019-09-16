628dede0dc92292a39b2750cd32f726bb18877c3
mondris360 <justmondris@gmail.com>
2019-09-16 22:47:31 +0100

    implemented the signup logic

--- controllers/frontend/signup.js
+++ controllers/frontend/signup.js
@@ -1,6 +1,7 @@
 // this module contains the login controller
 //DEPENDENCIES
 const mysql = require("../../models/database");
+const helpers = require("../helpers");
 
 
 exports.getPage = (req, res) =>{
@@ -9,17 +10,34 @@ exports.getPage = (req, res) =>{
 
 
 exports.postPage = async(req, res) =>{
+    let userID = await helpers.generateStr(3);
     let userDetails = {
-        id:85475,
+        id:userID,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         password: req.body.password
     }
-
+    console.log(userDetails)
     try {
-        const query = await mysql.query(`INSERT INTO users SET?`, userDetails);
+        const emailExists =  await mysql.query(`SELECT email From users WHERE email= ?`,[userDetails.email]);
+        if(emailExists[0].length > 0){
+            let message ='Email Address Already Exist';
+            let messageColor = 'red';
+            res.status(401);
+            res.render("frontend/signup", {message, messageColor});
+        } else {
+            const query = await mysql.query(`INSERT INTO users SET?`, userDetails);
+            let message ='Your Account Was Created Sucessfully';
+            let messageColor = 'blue';
+            res.status(200);
+            res.render("frontend/signup", {message, messageColor});
+        }
     } catch(err){
+        let message ='An Error Occured.Please Try Again'
+        let messageColor = 'red';
+        res.status(500);
+        res.render("frontend/signup", {message, messageColor});
         console.log(err);
     }
 }
--- controllers/helpers.js
+++ controllers/helpers.js
@@ -1,4 +1,24 @@
 // this module contain several helpers functions
 
 //DEPENDENCIES
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const cryptoRandonStr =  require("crypto-random-string");
+
+
+// generate crypto random string
+exports.generateStr = async function (length){
+    let str = cryptoRandonStr({length:length});
+    return str;
+}
+
+
+// //hash password
+// exports.hash = async(password)=>{
+//     try {
+//         let hashedPass =  await bcrypt.hash(password, saltRounds);
+//     }catch(err){
+//         console.log(err)
+//     }
+//     return hashedPass;
+// }
