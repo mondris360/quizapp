97daabfa5533108a212b122bc925edebfe538fc1
mondris360 <justmondris@gmail.com>
2019-09-19 12:14:51 +0100

    Implemented Payment Initialization

--- controllers/backend/paystack.js
+++ controllers/backend/paystack.js
@@ -18,20 +18,50 @@ exports.initPayment = async(req, res) =>{
         console.log("inside else");
         try {
             let query = await mysql.query(`SELECT email, firstName, lastName from users WHERE id=?`,[userID]);
-            let userInfo = query[0][0];
-            let email = userInfo.email;
-            let firstName = userInfo.firstName;
-            let lastName = user.Info.lastName;
+            let userData = query[0][0];
+            let email = userData.email;
+            let firstName = userData.firstName;
+            let lastName = userData.lastName;
             // convert the amount to kobo
-            let amount = amount * 100;
+            let amountInKobo = amount * 100;
+            console.log(amountInKobo)
             let userInfo = {
                 email,
                 firstName,
                 lastName,
-                amount
+                amount: amountInKobo
             }
+            let apiOption = {
+                url:'https://api.paystack.co/transaction/initialize',
+                method:"post",
+                body:JSON.stringify(userInfo),
+                headers: {
+                    authorization: 'Bearer sk_test_bf5e8d244c6ef6da2a8617e1c3f2e752a5fa40df',
+                    'content-type' : 'application/json',
+                    'cache-control': 'no-cache'
+                }
+            }
+            // initialize the payment
+            request(apiOption, (err, apiResponse,body)=>{
+                if(err){
+                    let message ="An Error Occured While Processing Your Payment";
+                    let messageColor = "red";
+                    console.log(err);
+                    res.status(500);
+                    res.redirect(`/dashboard?message=${message}&messageColor=${messageColor}`);
+                } else {
+                    let parsedBody = JSON.parse(body);
+                    let paymenturl =  parsedBody.data.authorization_url;
+                    res.status(200)
+                    res.redirect(paymenturl);
+                }
+            })
         }catch(err){
+            let message ="Unable To Process Your Payment";
+            let messageColor = "red";
+            res.status(500);
             console.log(err);
+            res.redirect(`/dashboard?message=${message}&messageColor=${messageColor}`);
         }
     } 
 }
